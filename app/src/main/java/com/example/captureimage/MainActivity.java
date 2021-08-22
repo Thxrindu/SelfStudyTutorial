7aeedfe7c1b09c902aa3bf6837ab8e3117fab426
Thxrindu <tharindunirmal996@gmail.com>
2021-08-22 15:29:17 +0530

    Camera - working

---
+++ app/src/main/java/com/example/captureimage/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.captureimage;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView iv1;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv1 = findViewById(R.id.imageview);
+        btn1 = findViewById(R.id.btnopen);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions( MainActivity.this,
+                    new String[]{
+                        Manifest.permission.CAMERA
+            },
+                    100);
+        }
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+
+            iv1.setImageBitmap(captureImage);
+
+        }
+    }
+}
